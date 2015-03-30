4cd8c7604661badd38b44e36addba68db5411d8f
antonioaurelio <antonioaurelioxyz@gmail.com>
2015-03-30 11:56:16 -0300

    Exclusão de xclock.ps1
    Exclusão de xclock, utilizar get-date.

--- xclock.ps1
+++
@@ -1,7 +0,0 @@
-function Get-xClock {    
-    Write-Output $([string]::Empty)
-    Write-Output "Clock"
-    Write-Output "-----"
-    Write-Host "$([System.DateTime]::Now.ToLongDateString()) $([System.DateTime]::Now.ToLongTimeString())"
-    Write-Output $([string]::Empty)    
-}
